4f1812ce53f46d09969f6c5c7d94f8f523eed375
Ans <anbilal@etisalat.ae>
2024-02-22 16:09:20 +0400

    static folder name in heroku settings changed

--- nstm/heroku_settings.py
+++ nstm/heroku_settings.py
@@ -16,7 +16,7 @@ MIDDLEWARE.insert(security_middleware_index + 1, 'whitenoise.middleware.WhiteNoi
 # STATIC FILES SETTINGS
 # ==============================================================================
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
