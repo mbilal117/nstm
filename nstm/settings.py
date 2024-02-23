a22428bce3a4abcb78514061fee56d0130a2bc36
Ans <anbilal@etisalat.ae>
2024-02-23 20:38:40 +0400

    static added

--- nstm/settings.py
+++ nstm/settings.py
@@ -130,7 +130,7 @@ LOCALE_PATHS = (
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'nstm/static'),
